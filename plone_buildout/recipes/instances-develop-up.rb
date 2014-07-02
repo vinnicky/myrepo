8a781e4ccaf1b6f11865892f39014438af85d44b
vinnicky <vin.thumati@gmail.com>
2014-07-02 14:54:36 +0530

    Revert "Revert "Added recipies""
    This reverts commit 0a199ca1d5f7a8ff7ee25a7a9436a0c649203620.

---
+++ plone_buildout/recipes/instances-develop-up.rb
@@ -0,0 +1,20 @@
+app_name =  node["plone_instances"]["app_name"]
+return if !app_name
+deploy = node[:deploy][app_name]
+# Don't run if deploy hasn't been run, or there's no mr.developer
+return if !deploy || deploy.empty? || !deploy[:deploy_to] || !::File.exists?(deploy[:deploy_to]) || !::File.exists?(::File.join(deploy[:deploy_to], 'current'))
+
+develop_bin = ::File.join(deploy[:deploy_to], 'current', 'bin', 'develop')
+execute "#{develop_bin} co -a '.*'" do
+  user deploy[:user]
+  group deploy[:group]
+  cwd  ::File.join(deploy[:deploy_to], 'current')
+  only_if 'test -e #{develop_bin}'
+end
+
+execute "#{develop_bin} up" do
+  user deploy[:user]
+  group deploy[:group]
+  cwd ::File.join(deploy[:deploy_to], 'current')
+  only_if "test -e #{develop_bin}"
+end
