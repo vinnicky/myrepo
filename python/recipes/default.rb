ab908709686fa11324b4e847f0fa6f58b91cc278
vinnicky <vin.thumati@gmail.com>
2014-07-03 12:57:23 +0530

    Changed python default.rb

--- python/recipes/default.rb
+++ python/recipes/default.rb
@@ -6,10 +6,14 @@
 # Just base packages required by the whole system here, please. Dependencies
 # for other recipes should live int hose recipes.
 
-node[:base_packages].each do |pkg|
-    package pkg do 
-        :upgrade
-    end
+node[:deploy].each do |application, deploy|
+  if deploy["custom_type"] != 'python'
+    next
+  end
+  python_base_setup do
+    deploy_data deploy
+    app_name application
+  end
 end
 
 ### Users/groups
