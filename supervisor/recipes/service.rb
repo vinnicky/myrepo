d666b1b20f951af0810908c5f9eda305088b1072
vinnicky <vin.thumati@gmail.com>
2014-07-11 16:44:48 +0530

    replaced

--- supervisor/recipes/service.rb
+++ supervisor/recipes/service.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Noah Kantrowitz <noah@opscode.com>
 # Cookbook Name:: supervisor
-# Resource:: service
+# Provider:: service
 #
 # Copyright:: 2011, Opscode, Inc <legal@opscode.com>
 #
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 actions :enable, :disable, :start, :stop, :restart, :reload
 
 def initialize(*args)
@@ -54,3 +53,56 @@ attribute :environment, :kind_of => Hash, :default => {}
 attribute :directory, :kind_of => [String, NilClass], :default => nil
 attribute :umask, :kind_of => [NilClass, String], :default => nil
 attribute :serverurl, :kind_of => String, :default => 'AUTO'
+
+action :enable do
+  execute "supervisorctl update" do
+    action :nothing
+    user "root"
+  end
+
+  template "#{node['supervisor']['dir']}/#{new_resource.service_name}.conf" do
+    source "program.conf.erb"
+    cookbook "supervisor"
+    owner "root"
+    group "root"
+    mode "644"
+    variables :prog => new_resource
+    notifies :run, resources(:execute => "supervisorctl update"), :immediately
+  end
+end
+
+action :disable do
+  execute "supervisorctl update" do
+    action :nothing
+    user "root"
+  end
+
+  file "#{node['supervisor']['dir']}/#{new_resource.service_name}.conf" do
+    action :delete
+    notifies :run, resources(:execute => "supervisorctl update"), :immediately
+  end
+end
+
+action :start do
+  execute "supervisorctl start #{new_resource.service_name}" do
+    user "root"
+  end
+end
+
+action :stop do
+  execute "supervisorctl stop #{new_resource.service_name}" do
+    user "root"
+  end
+end
+
+action :restart  do
+  execute "supervisorctl restart #{new_resource.service_name}" do
+    user "root"
+  end
+end
+
+action :reload  do
+  execute "supervisorctl restart #{new_resource.service_name}" do
+    user "root"
+  end
+end
