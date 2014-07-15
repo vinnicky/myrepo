8459eada2c1e90bfbceedd17e197ea56ba729a29
vinnicky <vin.thumati@gmail.com>
2014-07-15 18:31:30 +0530

    Update source.rb

--- python/recipes/source.rb
+++ python/recipes/source.rb
@@ -42,12 +42,4 @@ remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tar.bz2" do
   not_if { ::File.exists?(install_path) }
 end
 
-bash "build-and-install-python" do
-  cwd Chef::Config[:file_cache_path]
-  code <<-EOF
-  tar -jxvf Python-#{version}.tar.bz2
-  (cd Python-#{version} && ./configure #{configure_options})
-  (cd Python-#{version} && make && make install)
-  EOF
-  not_if { ::File.exists?(install_path) }
-end
+
