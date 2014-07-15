1488545ad979a7e5649271d6bed88ec784aa582c
vinnicky <vin.thumati@gmail.com>
2014-07-15 19:25:09 +0530

    Update source.rb

--- python/recipes/source.rb
+++ python/recipes/source.rb
@@ -1,23 +1,3 @@
-#
-# Author:: Seth Chisamore <schisamo@opscode.com>
-# Cookbook Name:: python
-# Recipe:: source
-#
-# Copyright 2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 configure_options = node['python']['configure_options'].join(" ")
 #make_options = node['python']['make_options'].join(" ")
 
@@ -42,11 +22,12 @@ remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tar.bz2" do
   not_if { ::File.exists?(install_path) }
 end
 
-
 bash "build-and-install-python" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
   tar -jxvf Python-#{version}.tar.bz2
+  (cd Python-#{version} && ./configure #{configure_options})
+  (cd Python-#{version} && make && make install)
   EOF
   not_if { ::File.exists?(install_path) }
 end
